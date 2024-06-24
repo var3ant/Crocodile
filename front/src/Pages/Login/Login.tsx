245d3fa83e447f26221143c03ea77df1b219d9f5
var3ant <92252693+var3ant@users.noreply.github.com>
2024-06-24 22:32:24 +0600

    Сделал чтобы кнопки авторизации/регистрации выключались пока не придет ответ от сервера

--- front/src/Pages/Login/Login.tsx
+++ front/src/Pages/Login/Login.tsx
@@ -1,18 +1,28 @@
 import React, {KeyboardEventHandler, useState} from "react";
 import {Button, Card, Input} from "antd";
 
-export type onClickFunc = (login: string, password: string) => void;
+export type onClickFunc = (login: string, password: string) => Promise<void>;
 
 
 export function Login(props: Readonly<{ onClick: onClickFunc, buttonText: string }>) {
     let [login, setLogin] = useState("");
     let [password, setPassword] = useState("");
+    let [isLoading, setLoading] = useState(false);
 
     const preventDefault = (e: any) => e.preventDefault();
     const onKeyUp = (e: any) => {
         if (e.key === 'Enter') {
-            props.onClick(login, password);
+            onClick();
         }
+    }
+
+    const onClick = () => {
+        if (isLoading) {
+            return;
+        }
+        setLoading(true);
+
+        props.onClick(login, password).finally(() => setLoading(false));
     }
 
 
@@ -22,12 +32,10 @@ export function Login(props: Readonly<{ onClick: onClickFunc, buttonText: string
                 <Input addonBefore='Name' placeholder="Enter name" value={login}
                        onChange={text => setLogin(text.target.value)}/>
                 <Input.Password addonBefore='Password' placeholder="Enter password" value={password}
-                                onChange={text => setPassword(text.target.value)}/>
-                <Button style={{marginLeft: 'auto'}} type="primary"
+                                onChange={e => setPassword(e.target.value)}/>
+                <Button loading={isLoading} style={{marginLeft: 'auto'}} type="primary"
                         onKeyDown={preventDefault}
-                        onClick={(e) => {
-                            props.onClick(login, password)
-                        }}
+                        onClick={onClick}
                 >{props.buttonText}</Button>
             </div>
         </Card>
